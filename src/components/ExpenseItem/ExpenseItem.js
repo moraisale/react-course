c79c4e0447fb87193e9c8349f9a45253c5a2281f
Alexandre Morais <alexandremorais26@gmail.com>
2021-06-01 17:25:12 -0300

    Atualizando

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
+
 function ExpenseItem() {
+  const expenseData = new Date(2021, 2, 28)
+  const expenseTitle = 'Car Insurance'
+  const expenseAmount = 294.67
   return(
 
     <div className="expense-item">
       <div>26/05/2021</div>
       <div className="expense-item__description">
-        <h2>Supermercado</h2>
-        <div className="expense-item__price">89.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
     </div>
   )
